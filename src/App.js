cb9b7ac85fc441b6eb3f145447d467a81b236d7d
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-09-16 20:57:31 +0700

    add CSS

--- src/App.js
+++ src/App.js
@@ -57,13 +57,15 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
         </Search>
+        </div>
         <Table
           list={list}
           searchTerm={searchTerm}
@@ -84,18 +86,25 @@ const Search = ({ value, onChange, children }) =>
   </form>
 
 const Table = ({ list, searchTerm, onDismiss }) =>
-  <div>
+  <div className="table">
     {list.filter(isSearched(searchTerm)).map(item =>
-      <div key={item.objectID}>
-        <span>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.num_comments}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.points}
+        </span>
+        <span style={{ width: '10%' }}>
           <Button
             onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
           >
             Dismiss
           </Button>
