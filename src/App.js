c296cee3f65f6c691079dba976b8c2c0b46cc32a
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-09-16 20:29:33 +0700

    create reusable Button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from './Button';
 
 const list = [
   {
@@ -103,12 +104,11 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
+              <Button
                 onClick={() => onDismiss(item.objectID)}
-                type="button"
               >
                 Dismiss
-              </button>
+              </Button>
             </span>
           </div>
         )}
